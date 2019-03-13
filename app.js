d075e2dd0a140cc23ebec16ffc0ff1d0b2c51716
Bryan Herbert <bryan77@twc.com>
2019-03-13 15:31:19 +0000

    add show route/show.ejs template

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.set("view engine", "ejs");
 // SCHEMA SETUP- will refactor later
 const campgroundSchema = new mongoose.Schema ({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 const Campground = mongoose.model("Campground", campgroundSchema);
@@ -18,7 +19,8 @@ const Campground = mongoose.model("Campground", campgroundSchema);
 /*Campground.create(
     {
         name: "Granite Hill", 
-        image: "https://farm9.staticflickr.com/8471/8137270056_21d5be6f52.jpg"
+        image: "https://farm9.staticflickr.com/8471/8137270056_21d5be6f52.jpg",
+        description: "This a huge granite hill.  No frills.  Just beautiful granite!"
     }, function(err, campground){
         if (err) {
             console.log(err);
@@ -33,22 +35,25 @@ app.get("/", function(req, res) {
     res.render("landing");
 });
 
+//INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res) {
     // Get all campgrounds from DB
     Campground.find({}, function(err, allCampgrounds) {
         if (err) {
             console.log(err);
         } else {
-             res.render("campgrounds", {campgrounds: allCampgrounds});
+             res.render("index", {campgrounds: allCampgrounds});
         }
     });
 });
 
+//CREATE - add new campground to DB
 app.post("/campgrounds", function(req, res) {
     //get data from form and add to campgrounds array
     const name = req.body.name;
     const image = req.body.image;
-    const newCampground = {name: name, image:image};
+    const desc = req.body.description;
+    const newCampground = {name: name, image:image, description: desc};
     // Create a new Campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated) {
         if (err) {
@@ -60,10 +65,25 @@ app.post("/campgrounds", function(req, res) {
     });
 });
 
+//NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res) {
     res.render("new");
 });
 
+//SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res) {
+    // find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+        }   else {
+            // render show template with that campground
+                res.render("show", {campground: foundCampground});
+
+        }
+    });
+    
+});
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The YelpCamp Server Has Started!");

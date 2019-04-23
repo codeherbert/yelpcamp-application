b8f5cda7939c2934dc47306636d1909f62dcdf34
Bryan Herbert <bryan77@twc.com>
2019-04-23 10:13:05 +0000

    delete feature for removing campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -67,18 +67,30 @@ router.get("/:id/edit", function (req, res) {
     });
 });
 
+// UPDATE CAMPGROUND ROUTE
 router.put("/:id", function(req, res) {
     //find and update the correct campground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
         if(err) {
             res.redirect("/campgrounds");
         } else {
+            //redirect somewhere(show page)
             res.redirect("/campgrounds/" + req.params.id);
         }
     });
-    //redirect somewhere(show page)
 });
-// UPDATE CAMPGROUND ROUTE
+
+// DESTROY CAMPGROUND ROUTE
+router.delete("/:id", function(req, res) {
+    Campground.findByIdAndRemove(req.params.id, function(err) {
+        if(err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
 // middleware
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()) {

b6f1e6071a15931a5e7e20e55dc5be9e79a3ec13
root <bryan77@twc.com>
2019-06-08 21:49:31 +0000

    add comment in app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/", indexRoutes);
 
-
+// Server connection
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The YelpCamp Server Has Started!");
 });
